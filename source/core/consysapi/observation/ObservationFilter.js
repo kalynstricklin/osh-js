523233370f1e81ff6e6da51733be6a93483a1e90
Kalyn Stricklin <kalynstricklin@botts-inc.com>
2025-05-01 15:51:39 -0500

    fixed query param names and added additional ones according to spec

--- source/core/consysapi/observation/ObservationFilter.js
+++ source/core/consysapi/observation/ObservationFilter.js
@@ -21,18 +21,25 @@ class ObservationFilter extends ConnectedSystemsApiFilter {
      * @param {Object} properties - object properties
      * @param {any} [properties.phenomenonTime='now'] - time range <00:00:00T00:00:00Z/00:00:00T00:00:00Z> | 'now' | 'latest'
      * @param {any} [properties.resultTime='now'] - time range <00:00:00T00:00:00Z/00:00:00T00:00:00Z> | 'latest'
+     * @param {string[]} [properties.dataStream=undefined] - Comma separated list of data stream local IDs or URIs to get observations for
+     * @param {string[]} [properties.system=undefined] - Comma separated list of systems local IDs or URIs to get observations for
+     * @param {string[]} [properties.observedProperty=undefined] - Comma separated list of observed property local IDs or URIs to get observations for
      * @param {string[]} [properties.foi=undefined] - Comma separated list of feature of interest IDs to get observations for
      * @param {string[]} [properties.select=undefined] - Comma separated list of properties to include or exclude from results (use "!" prefix to exclude)
      * @param {number[]} [properties.bbox=undefined] - BBOX to filter resources on their location
      * @param {string} [properties.location=undefined] - WKT geometry and operator to filter resources on their location or geometry
      * @param {string} [properties.format='application/json'] - Mime type designating the format to use to encode the response.
      * @param {string} [properties.replaySpeed=undefined] - Mime type designating the format to use to encode the response.
+
      */
     constructor(properties) {
         super({
             phenomenonTime: undefined,
             resultTime: undefined,
+            dataStream: undefined,
+            system: undefined,
             foi: undefined,
+            observedProperty: undefined,
             select: undefined,
             bbox: undefined,
             location: undefined,
