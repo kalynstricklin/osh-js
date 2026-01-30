bb50f707bb09ddedbc4ff2f096b79c392470c741
kalynstricklin <kalynstricklin@botts-inc.com>
2026-01-30 09:19:10 -0600

    commented out outputname nesting
    Need to make sure this doesnt break anything else

--- source/core/parsers/AbstractParser.js
+++ source/core/parsers/AbstractParser.js
@@ -133,16 +133,16 @@ class DataRecordParser extends AbstractParser {
     }
 
     parse(dataTypeParser, props, resultParent) {
-        if(!this.name) {
+        // if(!this.name) {
             super.parse(dataTypeParser, props, resultParent);
-        } else {
-            // parse size of the array
-            const result = {}
-            for (let parser of this.stack) {
-                parser.parse(dataTypeParser, props, result);
-            }
-            resultParent[this.name] = result;
-        }
+        // } else {
+        //     // parse size of the array
+        //     const result = {}
+        //     for (let parser of this.stack) {
+        //         parser.parse(dataTypeParser, props, result);
+        //     }
+        //     resultParent[this.name] = result;
+        // }
     }
 }
 
